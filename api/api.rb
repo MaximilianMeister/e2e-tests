880d05c7e6f05d7f55ef14a02b9a60a0cef44b37
Maximilian Meister <mmeister@suse.de>
2017-03-05 10:29:37 +0100

    api: enhance logging
    we lost some log output with the usage of Thread and tee
    
    Signed-off-by: Maximilian Meister <mmeister@suse.de>

--- api/api.rb
+++ api/api.rb
@@ -24,10 +24,13 @@ class RspecResultApi < Sinatra::Base
     def run!(env_vars={})
       env_vars.merge!({"VERBOSE" => "true"}).keep_if{ |_,v| ![nil, ""].include?(v) }
       env_vars_str = env_vars.map{ |k,v| "#{k}=#{v}" }.join(" ")
-      Thread.new do
-        Dir.chdir(settings.root) do
-          `#{env_var_str} bundle exec rspec --format json -o e2e-result.json spec/**/* 2>&1 | tee e2e-tests.log`
-        end
+      Dir.chdir(settings.root) do
+        pid = spawn(
+          "#{env_var_str} bundle exec rspec --format json -o e2e-result.json spec/**/*",
+          out: "e2e-tests.log",
+          err: "e2e-tests.log",
+        )
+        Process.detach(pid)
       end
     end
   end
